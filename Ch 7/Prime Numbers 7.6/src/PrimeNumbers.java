8e6396e1bd6175e403c421289c02f5aa54ecd7a7
Mikeyb8518 <110793646+Mikeyb8518@users.noreply.github.com>
2023-10-03 13:39:13 -0700

    Add files via upload

---
+++ Ch 7/Prime Numbers 7.6/src/PrimeNumbers.java
@@ -0,0 +1,35 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        final int NUMBER_OF_PRIMES = 50;
+
+        int[] primeNumbers = new int[NUMBER_OF_PRIMES];
+        int index = 0;
+
+        for (int i = 2; index <  NUMBER_OF_PRIMES; i++) {
+            if (isPrime(i)) {
+                primeNumbers[index++] = i;
+            }
+        }
+
+        printArray(primeNumbers, 10);
+    }
+
+    public static boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printArray(int [] array, int numberPerLines) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%10d", array[i]);
+
+            if ((i + 1) % 10 == 0) {
+                System.out.println("");
+            }
+        }
+    }
+}
